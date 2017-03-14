cbdc0fc59ac1497671db2e2e6f3b51fdbc3b7dc4
acp <alexcopa18@gmail.com>
2017-03-13 20:02:57 -0600

    depotacp

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
-
- include ActionController::Live
+  include ActionController::Live
   def download
     response.headers['Content-Type'] = 'text/plain'
     40.times do |i|
@@ -13,12 +11,8 @@ class ProductsController < ApplicationController
   ensure
     response.stream.close
   end
-
   # GET /products
   # GET /products.json
-  #def index
-   # @products = Product.all
-  #end
   def index
     @products = Product.all
     respond_to do |format|
@@ -31,22 +25,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
   end
-  
-  def who_bought
-    @product = Product.find(params[:id])
-    @latest_order = @product.orders.order(:updated_at).last
-    if stale?(@latest_order)
-      respond_to do |format|
-              format.html
-                      format.xml
 
-         format.xml { render xml: @product.to_xml(include: :orders) }
- format.atom
-         format.json { render json: @product.to_json(include: :orders) }
-
-      end
-    end
-  end
   # GET /products/new
   def new
     @product = Product.new
@@ -56,41 +35,56 @@ class ProductsController < ApplicationController
   def edit
   end
 
+
+
   # POST /products
   # POST /products.json
   def create
     @product = Product.new(product_params)
- 
+
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product,
-          notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created,
-          location: @product }
+        #format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        #format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
-        format.json { render json: @product.errors,
-          status: :unprocessable_entity }
+        #format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
- 
+
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product,
-          notice: 'Product was successfully updated.' }
+        #format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
- 
         @products = Product.all
         ActionCable.server.broadcast 'products',
-          html: render_to_string('store/index', layout: false)
+        html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit }
-        format.json { render json: @product.errors,
-          status: :unprocessable_entity }
+        #format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+
+      end
+    end
+  end
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.html
+        format.xml
+        format.atom
+        format.json { render json: @product.to_json(include: :orders) }
+
       end
     end
   end
@@ -99,21 +93,19 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url,
-          notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
- 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
- 
-    # Never trust parameters from the scary internet, only allow the white
-    # list through.
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
-end
+  end
